193a9d80662322a7a23d3990fd6a8a22e8caa922
liftes <793230022@qq.com>
2020-09-18 10:57:58 +0800

    更改了四皇后的代码注释，梳理了对应的逻辑结构。

--- 四皇后/main.cpp
+++ 四皇后/main.cpp
@@ -1,8 +1,9 @@
 #include<stdio.h>
 
-
+//用于统计解的数目的全局变量
 int count = 0;
 
+//用于判断棋子位置是否合法
 int isCorrect(int i, int j, int(*Q)[4])
 {
 	int s, t;
@@ -28,11 +29,11 @@ int isCorrect(int i, int j, int(*Q)[4])
 	return 1;//否则返回
 }
 
-
+//递归函数
 void Queue(int j, int(*Q)[4])
 {
 	int i, k;
-	if (j == 4) {//递归结束条件
+	if (j == 4) {//递归结束条件，即已经得到可行解，此时打印输出，否则跳过输出
 		for (i = 0; i < 4; i++) {
 			//得到一个解，在屏幕上显示
 			for (k = 0; k < 4; k++)
@@ -46,21 +47,22 @@ void Queue(int j, int(*Q)[4])
 	for (i = 0; i < 4; i++) {
 		if (isCorrect(i, j, Q)) {//如果Q[i][j]可以放置皇后
 			Q[i][j] = 1;//放置皇后
-			Queue(j + 1, Q);//递归深度优先搜索解空间树
-			Q[i][j] = 0;//这句代码就是实现回溯到上一层
+			Queue(j + 1, Q);//搜索下一层是否有可行解，如没有则返回上一层，如有则继续寻找，直到有输出被打印。
+			Q[i][j] = 0;//当前位置不放置皇后，即回溯到上一层
 		}
 	}
 }
-
 
 int main()
 {
 	int Q[4][4];
 	int i, j;
+	//地图初始化
 	for (i = 0; i < 4; i++)
 		for (j = 0; j < 4; j++)
 			Q[i][j] = 0;
+	//开始递归
 	Queue(0, Q);
-	printf("：：：The number of the answers are %d\n", count);
+	printf("可行解的数量为：%d\n", count);
 	return 0;
 }
